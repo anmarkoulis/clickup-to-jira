0570c4e5e4fa53982d4d83859b7128425afb9dd7
Antonis Markoulis <amarkoulis@hotmail.com>
2020-11-14 13:43:33 +0200

    feat(user-input): support user defined inputs for ticket types

--- clickup_to_jira/handlers/jira.py
+++ clickup_to_jira/handlers/jira.py
@@ -9,6 +9,102 @@ logger = getLogger(__name__)
 
 
 class JIRAHandler(JIRA):
+    status_mappings = {}
+    type_mappings = {}
+
+    def create_type_mappings(self, tickets):
+        """
+        Create mappings between clickup labels and Jira Ticket types.
+
+        :param list(Ticket) tickets: The tickets to create
+        """
+        clickup_labels = list(
+            set(
+                [
+                    ticket_type
+                    for ticket in tickets
+                    for ticket_type in ticket.type.split(",")
+                ]
+            )
+        )
+        jira_statuses = list(
+            set([issue_type.name for issue_type in self.issue_types()])
+        )
+        try:
+            default_jira_type = list(
+                filter(lambda x: "Story" == x, jira_statuses)
+            )[0]
+        except KeyError:
+            default_jira_type = jira_statuses[0]
+        default_mapping = {
+            clickup_label: default_jira_type
+            for clickup_label in clickup_labels
+        }
+        logger.info(f"Default Mapping : {default_mapping}")
+        selection = input(
+            f"Default mapping is : {default_mapping}"
+            f"\nPress Y if you want to use this mapping "
+            f"or N if you want to assign a mapping of your own."
+        )
+
+        if selection not in ["N", "Y"]:
+            while True:
+                selection = input(
+                    f"{selection} is not a valid choice. Please write Y on N."
+                )
+                if selection == "N":
+                    return self.__compute_type_mappings(
+                        clickup_labels, jira_statuses
+                    )
+                elif selection == "Y":
+                    return default_mapping
+
+        elif selection == "Y":
+            return default_mapping
+        else:
+            return self.__compute_type_mappings(clickup_labels, jira_statuses)
+
+    def __compute_type_mappings(self, clickup_labels, jira_statuses):
+        mappings = {}
+        for clickup_label in clickup_labels:
+            printable_label = clickup_label if clickup_label else "''"
+            jira_status = input(
+                f"Please provide a mapping for {printable_label} : "
+                f"\nEligible options are {jira_statuses}"
+            )
+            if jira_status not in jira_statuses:
+                while True:
+                    jira_status = input(
+                        f"{jira_status} is not a valid choice. "
+                        f"Please provide on of {jira_statuses}."
+                    )
+                    if jira_status in jira_statuses:
+                        mappings[clickup_label] = jira_status
+                        break
+            else:
+                mappings[clickup_label] = jira_status
+        return mappings
+
+    def create_tickets(self, tickets, project):
+        """
+        Create JIRA tickets:
+
+        :param list(Ticket) tickets: The tickets to create
+        :param str project: The project name
+        :return: The list of created JIRA issues
+        :rtype: list(jira.issue)
+        """
+        cur_project = list(
+            filter(lambda x: project in x.name, self.projects())
+        )[0]
+        self.type_mappings = self.create_type_mappings(tickets)
+        logger.info(self.type_mappings)
+
+        issues = list()
+        for ticket in tickets:
+            issues.append(self.create_ticket(ticket, cur_project.id))
+        return issues
+
     def create_ticket(self, ticket, project):
         """
         Create a JIRA ticket.
@@ -24,7 +120,9 @@ class JIRAHandler(JIRA):
             return
         issue_data = {
             "project": project,
-            "issuetype": {"name": ticket.type}
+            "issuetype": {
+                "name": self.type_mappings[ticket.type.split(",")[0]]
+            }
             if not ticket.parent
             else {"name": "Subtask"},
             "summary": ticket.title,
@@ -125,21 +223,3 @@ class JIRAHandler(JIRA):
         payload = {"accountId": assignee}
         self._session.put(url, data=json.dumps(payload))
         return True
-
-    def create_tickets(self, tickets, project):
-        """
-        Create JIRA tickets:
-
-        :param list(Ticket) tickets: The tickets to create
-        :param str project: The project name
-        :return: The list of created JIRA issues
-        :rtype: list(jira.issue)
-        """
-        cur_project = list(
-            filter(lambda x: project in x.name, self.projects())
-        )[0]
-
-        issues = list()
-        for ticket in tickets:
-            issues.append(self.create_ticket(ticket, cur_project.id))
-        return issues
